b1cd422a16766216801f4f227f5ad478ed43026b
santoshmondal <santosh.ece06@gmail.com>
2023-03-12 08:09:25 +0530

    with jwt. project init.

---
+++ src/main/java/in/cdac/MySpringBootProjectWithJWTApplication.java
@@ -0,0 +1,13 @@
+package in.cdac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBootProjectWithJWTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBootProjectWithJWTApplication.class, args);
+	}
+
+}
